e5859c44083865b45c3f653236ebd8d89340d83f
khyati soni <khyatisoni1102@gmail.com>
2023-02-04 23:16:11 +0530

    home

--- home.js
+++ home.js
@@ -48,7 +48,7 @@ jQuery(document).ready(function($){
 
 	// Account for home page with empty path
 	if ( path == '' ) {
-		path = 'index.html';
+		path = 'home.html';
 	}
 
 	var target = $('#navbarSupportedContent ul li a[href="'+path+'"]');
@@ -76,3 +76,17 @@ jQuery(document).ready(function($){
 //         }
 //     })
 // });
+'use strict';
+
+$(document).ready(function () {
+    $(window).bind('scroll', function (e) {
+        parallaxScroll();
+    });
+});
+
+function parallaxScroll() {
+    const scrolled = $(window).scrollTop();
+    $('#team-image').css('top', (0 - (scrolled * .20)) + 'px');
+    $('.img-1').css('top', (0 - (scrolled * .35)) + 'px');
+    $('.img-2').css('top', (0 - (scrolled * .05)) + 'px');
+}
